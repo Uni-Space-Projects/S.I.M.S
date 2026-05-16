0b6e9cef86f86f2e5629b28cb48b336f48e34942
Franco Nuzzo <franconuzzo2007@gmail.com>
2026-05-16 14:13:12 -0400

    create dto's

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,5 +1,8 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { UsersService } from './users.service';
+import {getBodyParserOptions} from "@nestjs/platform-express/adapters/utils/get-body-parser-options.util";
+import {LoginDto} from "./dto/Login.Dto";
+import {RegisterDto} from "./dto/Register.Dto";
 
 //Le decimos al programa que todas las rutas empiezan con users para mandarselas a las distintas clases
 @Controller('users')
@@ -9,8 +12,13 @@ export class UsersController {
     //Lo que le entrega la pagina al servicio (cuando alguien haga post en login)
     @Post('login')
     //Body agarra el json que envio el usuario al mandarle el formulario del login y lo guarda en una variable body
-    login(@Body() body: any) {
+    login(@Body() body: LoginDto) {
         //Llama al metodo login en usersService
         return this.usersService.login(body);
+    }
+
+    @Post('register')
+    register(@Body() body: RegisterDto) {
+        return this.usersService.register(body);
     }
 }
