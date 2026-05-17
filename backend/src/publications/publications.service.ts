b4811866b297630e8775869ee572a48f62e969b3
Franco Nuzzo <franconuzzo2007@gmail.com>
2026-05-17 16:36:23 -0400

    fixed database + test.http request create + create Publications

---
+++ backend/src/publications/publications.service.ts
@@ -0,0 +1,83 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Publication } from './publications.entity';
+import { CreatePublicationDto } from './Dto/create-publication.dto';
+import { UpdatePublicationDto } from './Dto/update-publication.dto';
+
+@Injectable()
+export class PublicationsService {
+    constructor(
+        @InjectRepository(Publication)
+        private readonly publicationRepository: Repository<Publication>,
+    ) {}
+
+    // 🔵 CREAR PUBLICACIÓN
+    async create(dto: CreatePublicationDto) {
+        const expiration = new Date(dto.expirationDate);
+
+        if (expiration <= new Date()) {
+            throw new Error('La fecha de vencimiento no puede ser pasada');
+        }
+
+        const publication = this.publicationRepository.create({
+            ...dto,
+            expirationDate: expiration,
+            isActive: true,
+        });
+
+        return await this.publicationRepository.save(publication);
+    }
+
+    // 🔵 OBTENER TODAS (solo activas y no vencidas)
+    async findAll() {
+        const today = new Date();
+
+        return this.publicationRepository.find({
+            where: {
+                isActive: true,
+            },
+        });
+    }
+
+    // 🔵 OBTENER UNA POR ID
+    async findOne(id: number) {
+        const publication = await this.publicationRepository.findOne({
+            where: { id },
+        });
+
+        if (!publication) {
+            throw new NotFoundException('Publicación no encontrada');
+        }
+
+        return publication;
+    }
+
+    // 🔵 ACTUALIZAR
+    async update(id: number, dto: UpdatePublicationDto) {
+        const publication = await this.findOne(id);
+
+        if (dto.expirationDate) {
+            const expiration = new Date(dto.expirationDate);
+
+            if (expiration <= new Date()) {
+                throw new Error('Fecha de vencimiento inválida');
+            }
+
+            publication.expirationDate = expiration;
+        }
+
+        Object.assign(publication, dto);
+
+        return this.publicationRepository.save(publication);
+    }
+
+    // 🔵 ELIMINAR (soft delete)
+    async remove(id: number) {
+        const publication = await this.findOne(id);
+
+        publication.isActive = false;
+
+        return this.publicationRepository.save(publication);
+    }
+}
--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {UserEntity} from "./users.entity";
 
 //Toda la informacion pasa por aqui y dicta que hace cada quien
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([UserEntity]), // 👈 REPOSITORIO REGISTRADO
+  ],
   //dice quien es el que va a dar los datos
   controllers: [UsersController],
   //dice quien va a recibir los datos
