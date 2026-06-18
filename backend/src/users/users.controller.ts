0dddbe4d4c3ce1c1b9c505eefddb5e80aedbfa86
Franco Nuzzo <franconuzzo2007@gmail.com>
2026-06-18 12:45:42 -0400

    memoria principal para la funcion de log in y nuevo modulo de dependencias para correr el proyecto

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -2,18 +2,31 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { LoginDto } from './dto/Login.Dto';
 import { RegisterDto } from './dto/Register.Dto';
+import { UserEntity } from './users.entity';
 
 //Le decimos al programa que todas las rutas empiezan con users para mandarselas a las distintas clases
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
+  private cacheUsuarios:UserEntity[] = [];
   //Lo que le entrega la pagina al servicio (cuando alguien haga post en login)
   @Post('login')
   //Body agarra el json que envio el usuario al mandarle el formulario del login y lo guarda en una variable body
-  login(@Body() body: LoginDto) {
+  async login(@Body() body: LoginDto) {
     //Llama al metodo login en usersService
-    return this.usersService.login(body);
+    for (const usuarios of this.cacheUsuarios) {
+      if (usuarios.email === body.email && usuarios.contrasena === body.password) {
+        return {
+          user: usuarios.id,
+          email: usuarios.email,
+
+        };
+      }
+    }
+      const nuevo = await this.usersService.login(body);
+      this.cacheUsuarios.push(nuevo);
+      return nuevo;
   }
 
   @Post('register')
